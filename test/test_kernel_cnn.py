9ddab353fb45a2aef10896616d9ad365d0bd159b
Damian Owerko <Damowerko@users.noreply.github.com>
2023-08-07 13:58:14 -0400

    update tests

--- test/test_kernel_cnn.py
+++ test/test_kernel_cnn.py
@@ -15,16 +15,14 @@ pytestmark = pytest.mark.parametrize(
 
 def random_input(batch_size, in_channels, in_kernels, n_dimensions):
     input_positions = torch.randn(batch_size, in_channels, in_kernels, n_dimensions)
-    input_weights = torch.randn(batch_size, in_channels, in_kernels)
+    input_weights = torch.randn(batch_size, in_channels, in_kernels, 1)
     return RKHS(input_positions, input_weights)
 
 
 def test_forward(batch_size, in_channels, in_kernels, n_dimensions):
-    input = random_input(
-        batch_size, in_channels, in_kernels, n_dimensions
-    )
+    input = random_input(batch_size, in_channels, in_kernels, n_dimensions)
     out_channels = 2
-    filter_kernels = 3 ** n_dimensions
+    filter_kernels = 3**n_dimensions
 
     # create a KernelConv layer with some example parameters
     layer = KernelConv(filter_kernels, in_channels, out_channels, n_dimensions)
@@ -43,6 +41,7 @@ def test_forward(batch_size, in_channels, in_kernels, n_dimensions):
         batch_size,
         out_channels,
         filter_kernels * in_channels * in_kernels,
+        1,
     )
     # check that the output is finite
     assert torch.isfinite(output_positions).all()
@@ -50,13 +49,14 @@ def test_forward(batch_size, in_channels, in_kernels, n_dimensions):
 
 
 def test_pool(batch_size, in_kernels, in_channels, n_dimensions):
-    input = random_input(
-        batch_size, in_channels, in_kernels, n_dimensions
-    )
-    out_kernels = 2
-    kernel = GaussianKernel()
-    nonlinearity = torch.nn.ReLU()
-    layer = KernelPool(out_kernels, kernel, nonlinearity)
+    input = random_input(batch_size, in_channels, in_kernels, n_dimensions)
+    max_kernels = 2
+    layer = KernelPool(max_kernels, strategy="largest")
     output_positions, output_weights = layer(input)
-    assert output_positions.shape == (batch_size, in_channels, out_kernels, n_dimensions)
-    assert output_weights.shape == (batch_size, in_channels, out_kernels)
+    assert output_positions.shape == (
+        batch_size,
+        in_channels,
+        max_kernels,
+        n_dimensions,
+    )
+    assert output_weights.shape == (batch_size, in_channels, max_kernels, 1)
