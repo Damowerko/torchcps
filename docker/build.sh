8a9fb2ddf68cb11c0ac4c80d7293ac0742360482
Damian Owerko <Damowerko@users.noreply.github.com>
2024-02-27 10:44:16 -0500

    build script refactor

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
+IMAGE_NAME="torchcps"
 export DOCKER_BUILDKIT=1
-docker pull docker.io/damowerko/motion-planning:latest || true
-docker build . -f ./docker/Dockerfile -t docker.io/damowerko/motion-planning --build-arg BUILDKIT_INLINE_CACHE=1 
-docker push docker.io/damowerko/motion-planning
+docker pull docker.io/damowerko/$IMAGE_NAME:latest || true
+docker build . -f ./docker/Dockerfile -t docker.io/damowerko/$IMAGE_NAME --build-arg BUILDKIT_INLINE_CACHE=1 
+docker push docker.io/damowerko/$IMAGE_NAME
