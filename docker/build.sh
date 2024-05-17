6a889fc72b24df5e7c05e3104707d58947c08363
Damian Owerko <damowerko@gmail.com>
2024-05-17 18:05:11 -0400

    update build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
+REGISTRY="lc1:32000"
+DOCKER_USER="damowerko"
 IMAGE_NAME="torchcps"
+IMAGE_TAG="latest"
+DOCKER_URI="$REGISTRY/$DOCKER_USER/$IMAGE_NAME:$IMAGE_TAG"
 export DOCKER_BUILDKIT=1
-docker pull docker.io/damowerko/$IMAGE_NAME:latest || true
-docker build . -f ./docker/Dockerfile -t docker.io/damowerko/$IMAGE_NAME --build-arg BUILDKIT_INLINE_CACHE=1 
-docker push docker.io/damowerko/$IMAGE_NAME
+docker pull $DOCKER_URI || true
+docker build . -f ./docker/Dockerfile -t $DOCKER_URI --build-arg BUILDKIT_INLINE_CACHE=1 
+docker push $DOCKER_URI
